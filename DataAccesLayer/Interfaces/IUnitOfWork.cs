e075e76b50ed15c9cb65fc4e3ef663727e2b7e1f
SamandarbekYr <SamandarbekYR@gmail.com>
2024-04-17 18:33:59 +0500

    DataAccesLayer: UnitOfWork Created

---
+++ DataAccesLayer/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using DataAccesLayer.Interfaces.Branches;
+using DataAccesLayer.Interfaces.Categories;
+using DataAccesLayer.Interfaces.Products;
+using DataAccesLayer.Interfaces.Receipts;
+using DataAccesLayer.Interfaces.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public IUserRole UserRole { get; set; }
+        public IUser User { get; set; }
+        public ICategory Category { get; set; }
+        public IProduct Product { get; set; }
+        public IProductItem ProductItem { get; set; }
+        public IReceipt Receipt { get; set; }
+        public IReceiptItem ReceiptItem { get; set; }
+        public IBranch Branch { get; set; }
+    }
+}
---
+++ DataAccesLayer/Repositories/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using DataAccesLayer.Data;
+using DataAccesLayer.Interfaces;
+using DataAccesLayer.Interfaces.Branches;
+using DataAccesLayer.Interfaces.Categories;
+using DataAccesLayer.Interfaces.Products;
+using DataAccesLayer.Interfaces.Receipts;
+using DataAccesLayer.Interfaces.Users;
+using DataAccesLayer.Repositories.Branches;
+using DataAccesLayer.Repositories.Categories;
+using DataAccesLayer.Repositories.Products;
+using DataAccesLayer.Repositories.Receipts;
+using DataAccesLayer.Repositories.Users;
+using Domian.Entities.Receipts;
+
+namespace DataAccesLayer.Repositories
+{
+    public class UnitOfWork(AppDbContext appDb) : IUnitOfWork
+    {
+        public IUserRole UserRole { get; set; } = new UserRoleRepository(appDb);
+        public IUser User {  get; set; } = new UserRepository(appDb);
+        public ICategory Category { get; set; } = new CategoryRepository(appDb);
+        public IProduct Product { get; set; } = new ProductRepository(appDb);
+        public IProductItem ProductItem {  get; set; } = new ProductItemRepository(appDb);
+        public IReceipt Receipt { get; set; } = new ReceiptRepository(appDb);
+        public IReceiptItem ReceiptItem { get; set; } = new ReceiptItemsRepository(appDb);
+        public IBranch Branch { get; set; } = new BranchRepository(appDb);
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
